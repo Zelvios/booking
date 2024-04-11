0345ffbbcd27ba5b908c2ab03ec960281b731ecf
Jaco296c <jaco296c@outlook.dk>
2024-04-11 14:45:41 +0200

    Added diesel.

---
+++ api/src/db/schema.rs
@@ -0,0 +1,55 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    admin (id) {
+        id -> Int4,
+        user_id -> Int4,
+    }
+}
+
+diesel::table! {
+    booking (id) {
+        id -> Int4,
+        user_id -> Int4,
+        schedule_id -> Int4,
+    }
+}
+
+diesel::table! {
+    booking_schedule (booking_id, schedule_id) {
+        booking_id -> Int4,
+        schedule_id -> Int4,
+    }
+}
+
+diesel::table! {
+    schedule (id) {
+        id -> Int4,
+        user_id -> Int4,
+    }
+}
+
+diesel::table! {
+    user (id) {
+        id -> Int4,
+        name -> Varchar,
+        #[max_length = 60]
+        password -> Varchar,
+        created -> Timestamp,
+    }
+}
+
+diesel::joinable!(admin -> user (user_id));
+diesel::joinable!(booking -> schedule (schedule_id));
+diesel::joinable!(booking -> user (user_id));
+diesel::joinable!(booking_schedule -> booking (booking_id));
+diesel::joinable!(booking_schedule -> schedule (schedule_id));
+diesel::joinable!(schedule -> user (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    admin,
+    booking,
+    booking_schedule,
+    schedule,
+    user,
+);
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use crate::db::establish_connection;
+use crate::routes::user::hello;
+
+mod db;
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
